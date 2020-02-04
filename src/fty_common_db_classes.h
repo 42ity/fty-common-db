a9bfdbbc7df6b9759d593f277fa49796dbb90e4c
Clappier, Eric <EricClappier@eaton.com>
2020-02-04 16:23:14 +0100

    Regenerated zproject

--- src/fty_common_db_classes.h
+++ src/fty_common_db_classes.h
@@ -32,9 +32,9 @@
 //  External API
 #include "../include/fty_common_db.h"
 
-//  Opaque class structures to allow forward references
-
 //  Extra headers
+
+//  Opaque class structures to allow forward references
 
 //  Internal API
 
